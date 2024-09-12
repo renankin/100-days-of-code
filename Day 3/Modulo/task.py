9ec4366175dfed7044891e5d321a960ac379d922
renankin <renankin@outlook.com>
2024-09-12 13:13:01 +0100

    Halfway Day 3

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,8 @@
+print(10 % 3)  # the remainder of this operation is 1
+
+number = int(input("Type a number: "))
+
+if number % 2 != 0:
+    print("This number is odd.")
+else:
+    print("This number is even.")
--- Day 3/Multiple Ifs/task.py
+++ Day 3/Multiple Ifs/task.py
@@ -5,10 +5,20 @@ if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
     if age <= 12:
+        bill = 5
         print("Please pay $5.")
     elif age <= 18:
+        bill = 7
         print("Please pay $7.")
     else:
+        bill = 12
         print("Please pay $12.")
+
+    wants_photo = input("Do you want photos taken? Reply 'yes' or 'no': ")
+
+    if wants_photo == "yes":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry you have to grow taller before you can ride.")
