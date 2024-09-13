86d2dfbafa4a4296a78654f7306f73b2c89d68a0
renankin <renankin@outlook.com>
2024-09-13 17:44:28 +0100

    Completed Logical Operators and Python Pizza on Day 3

--- Day 3/Python Pizza/solution.py
+++ Day 3/Python Pizza/solution.py
@@ -3,7 +3,7 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
-# todo: work out how much they need to pay based on their size choice.
+# work out how much they need to pay based on their size choice.
 
 bill = 0
 
@@ -16,14 +16,14 @@ elif size == "L":
 else:
     print("You have chosen an invalid size.")
 
-# todo: work out how much to add to their bill based on their pepperoni choice.
+# work out how much to add to their bill based on their pepperoni choice.
 if pepperoni == "Y":
     if size == "S":
         bill += 2
     else:
         bill += 3
 
-# todo: work out their final amount based on whether if they want extra cheese.
+#  work out their final amount based on whether if they want extra cheese.
 if extra_cheese == "Y":
     bill += 1
 
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,26 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+# work out how much they need to pay based on their size choice.
+price = 0
+if size == "S":
+    price += 15
+elif size == "M":
+    price += 20
+elif size == "L":
+    price += 25
+else:
+    print("You typed a wrong input.")
+
+# work out how much to add to their based on their pepperoni choice.
+if pepperoni == "Y":
+    if size == "S":
+        price += 2
+    else:
+        price += 3
+
+# work out their final amount based on whether they want extra cheese.
+if extra_cheese == "Y":
+    price += 1
+
+print(f"Your final bill is: ${price}.")
