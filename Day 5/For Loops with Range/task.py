35cb4ed733f078edfd189ba5f623fd5d99f98ea4
renankin <renankin@outlook.com>
2024-09-14 21:27:34 +0100

    Completed Day 5

---
+++ Day 5/For Loops with Range/task.py
@@ -0,0 +1,10 @@
+# Range function with For Loop
+for number in range(1, 11, 3):
+    print(number)
+
+# The Gauss Challenge
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)
