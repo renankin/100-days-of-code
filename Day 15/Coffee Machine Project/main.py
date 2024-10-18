f2b78472e490307513fed0212870b2e3c4522a35
renankin <renankin@outlook.com>
2024-10-18 15:26:09 +0100

    Complete Day 15

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -35,29 +35,18 @@ resources = {
 
 # 2. Turn off the Coffee Machine by entering "off" to the prompt.
 
-
 # 3. Print report.
-def output_resources():
-    """Takes a dictionary of resources database and prints the available
-    resources"""
-
-    print(f"Water: {resources["water"]}ml")
-    print(f"Milk: {resources["milk"]}ml")
-    print(f"Coffe: {resources["coffee"]}g")
-    print(f"Money: ${resources["money"]}")
 
 
-# Check resources sufficient?
-def check_resources(coffee_choice):
+# 4. Check resources sufficient?
+def check_resources(order_ingredients):
     """Takes a dictionary of resources database and checks if there are
     available ingredients to make the coffee. Returns True if there are
     enough ingredients and false if not."""
 
-    recipe = MENU[coffee_choice]["ingredients"]
-
-    for ingredient in recipe:
-        if resources[ingredient] < recipe[ingredient]:
-            print(f"Sorry there is not enough {ingredient}")
+    for item in order_ingredients:
+        if resources[item] < order_ingredients[item]:
+            print(f"Sorry there is not enough {item}")
             return False
 
     return True
@@ -69,31 +58,24 @@ def process_coins():
 
     print("Please insert coins")
 
-    quarters = int(input("How many quarters?: "))
-    dimes = int(input("How many dimes?: "))
-    nickles = int(input("How many nickles?: "))
-    pennies = int(input("How many pennies?: "))
-
-    total = (+ 0.25 * quarters
-             + 0.10 * dimes
-             + 0.05 * nickles
-             + 0.01 * pennies)
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.10
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
 
     return total
 
 
 # 6. Check transaction successful?
-def check_funds(inserted_coins, coffee_choice):
+def check_funds(money_received, coffee_cost):
     """Takes the inserted money and coffee choice and returns True if the
     money is enough and False if not."""
 
-    coffee_cost = MENU[coffee_choice]["cost"]
-
-    if inserted_coins >= coffee_cost:
-        resources["money"] += coffee_cost
-        if inserted_coins > coffee_cost:
-            change = inserted_coins - coffee_cost
+    if money_received >= coffee_cost:
+        if money_received > coffee_cost:
+            change = money_received - coffee_cost
             print(f"Here is ${change:.2f} in change.")
+        resources["money"] += coffee_cost
         return True
     else:
         print("Sorry that's not enough money. Money refunded.")
@@ -101,34 +83,37 @@ def check_funds(inserted_coins, coffee_choice):
 
 
 # 7. Make coffee.
-def make_coffee(coffee_choice):
-    """Takes the coffee choice and deduct the value from the resources"""
+def make_coffee(coffee_choice, order_ingredients):
+    """Takes the coffee choice and order ingredients and deduct the value from
+    the
+    resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
 
-    recipe = MENU[coffee_choice]["ingredients"]
-
-    for ingredient in recipe:
-        resources[ingredient] -= recipe[ingredient]
-
-    print(f"Here is your {coffee_choice}. Enjoy!")
+    print(f"Here is your {coffee_choice} ☕. Enjoy!")
 
 
 def coffee_machine():
-    user_choice = ""
 
-    while user_choice != "off":
+    user_choice = input(
+        "What would like? (espresso/latte/cappuccino) ").lower()
 
-        user_choice = input(
-            "What would like? (espresso/latte/cappuccino) ").lower()
-
-        if user_choice == "report":
-            output_resources()
-        elif user_choice == "off":
-            return
-        else:
-            if check_resources(user_choice):
-                user_coins = process_coins()
-                if check_funds(user_coins, user_choice):
-                    make_coffee(user_choice)
+    if user_choice == "report":
+        print(f"Water: {resources["water"]}ml")
+        print(f"Milk: {resources["milk"]}ml")
+        print(f"Coffe: {resources["coffee"]}g")
+        print(f"Money: ${resources["money"]}")
+        coffee_machine()
+    elif user_choice == "off":
+        return
+    else:
+        coffee_ingredients = MENU[user_choice]["ingredients"]
+        if check_resources(coffee_ingredients):
+            payment = process_coins()
+            coffe_cost = MENU[user_choice]["cost"]
+            if check_funds(payment, coffe_cost):
+                make_coffee(user_choice, coffee_ingredients)
+        coffee_machine()
 
 
 coffee_machine()
