c7d445a9635d069998808a172e052922d7e59330
renankin <renankin@outlook.com>
2024-10-29 18:30:13 +0000

    Complete Day 17

--- Day 17/quiz-game-start/main.py
+++ Day 17/quiz-game-start/main.py
@@ -4,10 +4,13 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for item in question_data:
-    new_question = Question(item["text"], item["answer"])
+    new_question = Question(item["question"], item["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You completed the quiz.")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
--- Day 17/quiz-game-start/quiz_brain.py
+++ Day 17/quiz-game-start/quiz_brain.py
@@ -4,21 +4,38 @@
 
 # TODO: checking if we're the end of the quiz
 
+# Create a method called still_has_questions
+
+# Return a boolean depending on the value of question_number
+
+# Use the while loop to show the next question until the end.
+
 class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
-        current_question = self.question_list[self.question_number]
-        if answer == current_question.answer:
-            return True
-        else:
-            return False
+        return self.question_number < len(self.question_list)
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        answer = input(f"Q.{self.question_number}: "
-                       f"{current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: "
+                            f"{current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
+
+
