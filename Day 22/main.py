07abef55683210e05d71956915d318c6957af460
renankin <renankin@outlook.com>
2024-11-07 22:38:53 +0000

    Complete Day 22 Pong game

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -20,6 +23,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -27,10 +31,22 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    # Detect collision with right paddle
+    # Detect collision with paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 330 or ball.distance(
             l_paddle) < 50 and ball.xcor() < -330:
         ball.bounce_x()
+
+    # Detect when right paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_score += 1
+        scoreboard.update_scoreboard()
+
+    # Detect when left paddle misses the ball:
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_score += 1
+        scoreboard.update_scoreboard()
 
 
 screen.exitonclick()
