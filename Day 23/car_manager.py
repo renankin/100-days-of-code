50caaa53b9382dc568d832434d12b2273244207c
renankin <renankin@outlook.com>
2024-11-08 19:26:02 +0000

    Generate new cars and include collision with car

--- Day 23/car_manager.py
+++ Day 23/car_manager.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from random import choice, randint
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
@@ -10,3 +11,11 @@ class CarManager(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("square")
+        self.setheading(180)
+        self.shapesize(stretch_len=2)
+        self.color(choice(COLORS))
+        self.penup()
+        self.goto((280, randint(-250, 250)))
+
+    def move(self):
+        self.forward(STARTING_MOVE_DISTANCE)
--- Day 23/main.py
+++ Day 23/main.py
@@ -1,19 +1,38 @@
 from turtle import Screen
 import time
 from player import Player
+from car_manager import CarManager
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+cars = []
 
 screen.listen()
 screen.onkey(fun=player.move, key="Up")
 
 game_is_on = True
+loops = 1
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if loops % 6 == 0:
+        new_car = CarManager()
+        cars.append(new_car)
+
+    for car in cars:
+        car.move()
+
+        # Detect collision with car
+        if player.distance(car) < 15:
+            game_is_on = False
+
+    # Detect if player reached
+
+    loops += 1
 
 screen.exitonclick()
