2683d556e0531a290e01cb2a0342afdbbd8ec53b
renankin <renankin@outlook.com>
2024-11-29 01:21:07 -0500

    Day 32 - Sending emails with Python

---
+++ day-032/birthday-wisher/main.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import pandas
+from random import randint
+import smtplib
+
+MY_EMAIL = "pythonisfun0@gmail.com"
+MY_PASSWORD = "invxmtboznohslmo"
+
+now = datetime.now()
+today_tuple = (now.month, now.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {
+    (data_row.month, data_row.day): data_row for
+    (index, data_row) in data.iterrows()
+}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{randint(1, 3)}.txt"
+    with open(file_path) as file:
+        contents = file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n{contents}"
+        )
---
+++ day-032/quote-sender/main.py
@@ -0,0 +1,22 @@
+import smtplib
+import random
+import datetime as dt
+
+MY_EMAIL = "pythonisfun0@gmail.com"
+MY_PASSWORD = "invxmtboznohslmo"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 3:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Quote of the day\n\n{quote}"
+        )
