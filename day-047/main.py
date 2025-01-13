d0e5fafbbaf9ce46550c7499f405993fdaa0df83
renankin <renankin@outlook.com>
2025-01-13 12:37:41 +0000

    Day 47 - Scrape Amazon live site with headers

--- day-047/main.py
+++ day-047/main.py
@@ -4,28 +4,39 @@ import smtplib
 from dotenv import load_dotenv
 import os
 
-# Load variables for SMTP
 load_dotenv()
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-SMTP_ADDRESS = os.environ.get("SMTP_ADDRESS")
 
-# Scrape the website to get the price
-amazon_url = "https://appbrewery.github.io/instant_pot/"
-response = requests.get(url=amazon_url)
+# Scrape the website to get the item price and title
+amazon_url = ("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_"
+              "=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
+header = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"
+              "*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-GB,en;q=0.9",
+    "Priority": "u=0, i",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+                  "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1.1 "
+                  "Safari/605.1.15",
+}
+response = requests.get(url=amazon_url, headers=header)
 soup = BeautifulSoup(markup=response.content, features="html.parser")
-item_title = soup.find(id="productTitle").getText()
+item_title = soup.find(id="productTitle").getText().strip()
 price_text = soup.find(class_="a-offscreen").getText()
-price_float = float(price_text.split("$")[1])
+price_as_float = float(price_text.split("$")[1])
 
 # Send email notification
-if price_float < 100:
-    with smtplib.SMTP(SMTP_ADDRESS) as connection:
+BUY_PRICE = 100
+if price_as_float < BUY_PRICE:
+    message = f"{item_title} is now {price_text}"
+    with smtplib.SMTP(os.environ.get("SMTP_ADDRESS")) as connection:
         connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=MY_EMAIL,
-            msg=f"Amazon Price Alert!\n\n{item_title} is now {price_text} "
-                f"{amazon_url}"
-        )
+        connection.login(user=os.environ.get("MY_EMAIL"),
+                         password=os.environ.get("MY_PASSWORD"))
+        connection.sendmail(from_addr=os.environ.get("MY_EMAIL"),
+                            to_addrs=os.environ.get("MY_EMAIL"),
+                            msg=f"Subject:Amazon Price Alert!\n\n"
+                                f"{message}\n{amazon_url}".encode("utf-8"))
